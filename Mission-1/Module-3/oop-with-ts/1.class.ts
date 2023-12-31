7407974c5c0098e74bdd9038d39b2079ce9bd24f
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-12-31 12:59:12 +0600

    [Mission-1]: Be a Typescript Technocrat Revise

--- Mission-1/Module-3/oop-with-ts/1.class.ts
+++ Mission-1/Module-3/oop-with-ts/1.class.ts
@@ -4,19 +4,19 @@ class Animal {
   // public name: string;
   // public species: string;
   // public sound: string;
-  // 4. constructor
+  // 3. constructor
   constructor(public name: string, public species: string, public sound: string) {
     // this.name = name;
     // this.species = species;
     // this.sound = sound;
   }
-  // 5. method
+  // 4. method
   makeSound() {
     console.log(`The ${this.name} says ${this.sound}`);
   }
 }
 
-// 6. create object or instance
+// 5. create object or instance
 const cat = new Animal("Billurany", "Cat", "Meaw Meaw");
 console.log(cat.name);
 
